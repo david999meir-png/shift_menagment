0fa4ec25babf96d858ce5bda0a26aeb3ba08b16c
david999meir-png <david999meir@gmail.com>
2026-05-20 22:34:46 +0300

    adding func: handle add soldie

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from soldier_manager import add_soldier, get_all_soldiers
+
+
 def show_menu() -> None:
     print("====================")
     print("-----Main menu-----")
@@ -10,3 +13,14 @@ def show_menu() -> None:
 def get_user_choice() -> str | int:
     choice = input("enter your choice:\n")
     return choice
+
+
+def handle_add_soldier() -> None:
+    try:
+        soldier_id = input("ehter soldier id:\n")
+        soldier_name = input("enter soldier name:\n")
+        
+        add_soldier(soldier_id, soldier_name)
+
+    except ValueError as e:
+        print(e)
