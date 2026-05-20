591d30a2b6ddfa188811f17da4e96d50f6627bca
david999meir-png <david999meir@gmail.com>
2026-05-20 22:20:18 +0300

    adding func: show menu

---
+++ main.py
@@ -0,0 +1,8 @@
+def show_menu() -> None:
+    print("====================")
+    print("-----Main menu-----")
+    print("1. soldier menager")
+    print("2. duties menager")
+    print("3. EXIT")
+    print("====================")
+    
